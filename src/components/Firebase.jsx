49734921b61bc352fcb2b89bc8f75332bc26ec12
ezekielkibiego <kibiezekiel@gmail.com>
2024-11-29 12:05:50 +0300

    configure firebase creds

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0Lvud6fXkRmuBySEDt5oZz9Ucoryl0OM",
-    authDomain: "market-place-997a9.firebaseapp.com",
-    projectId: "market-place-997a9",
-    storageBucket: "market-place-997a9.firebasestorage.app",
-    messagingSenderId: "1076027274357",
-    appId: "1:1076027274357:web:c450c58c0f20f4f7f0343c",
-    measurementId: "G-XHB3E2C5QL"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
   };
 
 const app = initializeApp(firebaseConfig);
